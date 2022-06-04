ef87554659d405d593ffc80278c9b73d04a4fed7
chizeni24 <zvy321@my.utsa.edu>
2022-06-04 02:12:46 -0500

    Portfolio complete

--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React,{useState} from 'react'
 import { FaBars, FaTimes,FaGithub, FaLinkedin } from 'react-icons/fa'
-import Logo from '../assets/logo.png'
+import Logo from '../assets/EIlogo.png'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
+import {Link} from 'react-scroll'
+
+
 const Navbar = () => {
   const [nav, setNav] = useState(false)
   const handleClick = () => setNav(!nav)
@@ -15,11 +18,31 @@ const Navbar = () => {
 
       {/* menu */}
         <ul className='hidden md:flex'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contacts</li>
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+            Home
+            </Link>
+          </li>
+          <li>
+            <Link to='about' smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to='skills' smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to='work' smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link to='contact' smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
         </ul>
 
       {/*Hamburger*/}
@@ -29,23 +52,43 @@ const Navbar = () => {
 
       {/*Mobile menu*/}
       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'} >
-        <li className= 'py-6 text-4xl'>Home</li>
-        <li className= 'py-6 text-4xl'>About</li>
-        <li className= 'py-6 text-4xl'>Skills</li>
-        <li className= 'py-6 text-4xl'>Work</li>
-        <li className= 'py-6 text-4xl'>Contacts</li>
+        <li className= 'py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li  className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li  className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/*Social icons*/}
       <div className = 'hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className= 'w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-800'>
-            <a className= 'flex justify-between items-center w-full text-gray-300' href="/"> 
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/echizeni-ikpah-562844b4/"> 
             LinkedinIn  <FaLinkedin size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-900'>
-            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/chizeni24">
             Github <FaGithub size={30} />
             </a>
           </li>
